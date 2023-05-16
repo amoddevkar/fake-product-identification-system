a4b1932f642545fe89aeb4b3570505a3853f3961
amoddevkar <87513080+amoddevkar@users.noreply.github.com>
2023-05-17 01:32:45 +0530

    added test cases

--- test/FPIS_test.js
+++ test/FPIS_test.js
@@ -1,11 +1,13 @@
 const { expect } = require("chai");
 
-const COMPANYNAME = "Ryzen"
+const PRODUCTNAME = "Ryzen"
 const PRICE = 100
 const ID = "testid"
 const PRODUCTIMG = "testimg"
 const PRODUCTQR = "testqr"
-
+const USERNAME = "testuser123"
+const EMAIL = "test@test.test"
+const ROLE = "Manufacturer"
 
 describe("FPIS_contract", () => {
 
@@ -25,87 +27,127 @@ describe("FPIS_contract", () => {
       expect(await product.contractOwner()).to.equal(deployer.address)
     })
 
-    // it("Set owner roles mapping", async () => {
-    //   expect(await product.roleNames["Manufacturer"].to.equal("Manufacturer"))
-    // })
   })
 
-  describe("AddProduct", () => {
+  describe("UserRegistration", () => {
 
     let transaction1, transaction2;
 
     beforeEach(async () => {
+      // add user
+      transaction1 = await product.connect(deployer).addUser(USERNAME, EMAIL, ROLE)
+      await transaction1.wait()
+
+    })
+
+    it("Returns user data", async () => {
+      transaction2 = await product.connect(deployer).getUser()
+      const user = { username: transaction2[0], email: transaction2[1], role: transaction2[2], address: transaction2[3] }
+
+      expect(user.username).to.equal(USERNAME)
+      expect(user.email).to.equal(EMAIL)
+      expect(user.role).to.equal(ROLE)
+      expect(user.address).to.equal(deployer.address)
+    })
+
+    it("Emits add user event", () => {
+      expect(transaction1).to.emit(product, "AddUser")
+    })
+
+  })
+
+  describe("AddProduct", () => {
+
+    let transaction0, transaction1, transaction2;
+
+    beforeEach(async () => {
       // add a product
-      transaction1 = await product.connect(deployer).addProduct(COMPANYNAME, PRICE, ID, PRODUCTIMG, PRODUCTQR)
+      transaction0 = await product.connect(deployer).addUser(USERNAME, EMAIL, ROLE)
+      await transaction0.wait()
+      transaction1 = await product.connect(deployer).addProduct(PRODUCTNAME, PRICE, ID, PRODUCTIMG, PRODUCTQR)
       await transaction1.wait()
 
     })
 
     it("Returns product attributes", async () => {
       transaction2 = await product.connect(buyer).getProduct(ID)
-      const pro = { id: transaction2[0], companyName: transaction2[1], price: transaction2[2], productImg: transaction2[3], productQr: transaction2[4], currentOwner: transaction2[5], owners: transaction2[6], autheticate: transaction2[7] }
-
+      const pro = { id: transaction2[0], productName: transaction2[1], price: transaction2[2], productImg: transaction2[3], productQr: transaction2[4], currentOwner: transaction2[5], owners: transaction2[6], autheticate: transaction2[7] }
+      const proOnwersArray = [pro.owners[0].userAdd]
       expect(pro.id).to.equal(ID)
-      expect(pro.companyName).to.equal(COMPANYNAME)
+      expect(pro.productName).to.equal(PRODUCTNAME)
       expect(pro.price).to.equal(PRICE)
       expect(pro.productImg).to.equal(PRODUCTIMG)
       expect(pro.productQr).to.equal(PRODUCTQR)
       expect(pro.currentOwner).to.equal(deployer.address)
-      expect(pro.owners[0].userAdd).to.have.ordered.members(deployer.address)
+      expect(proOnwersArray).to.have.ordered.members([deployer.address])
       expect(pro.autheticate).to.equal(false)
     })
 
-    it("Emits add event", () => {
+    it("Emits add product event", () => {
       expect(transaction1).to.emit(product, "Add")
     })
   })
 
   describe("UpdateProduct", () => {
-    let transaction1, transaction2, transaction3;
+    let transaction, transaction0, transaction1, transaction2, transaction3;
 
     beforeEach(async () => {
-      //update a product
-      transaction1 = await product.connect(deployer).addProduct(COMPANYNAME, PRICE)
+      // add users
+      transaction = await product.connect(deployer).addUser(USERNAME, EMAIL, ROLE)
+      await transaction.wait()
+      transaction0 = await product.connect(buyer).addUser("buyer", "buyer@gamil.com", "Customer")
+      await transaction0.wait()
+      // add a product
+      transaction1 = await product.connect(deployer).addProduct(PRODUCTNAME, PRICE, ID, PRODUCTIMG, PRODUCTQR)
       await transaction1.wait()
-      transaction2 = await product.connect(deployer).updateProductData(ID, buyer.address)
+      //update a product
+      transaction2 = await product.connect(deployer).updateOwner(ID, buyer.address)
       await transaction2.wait()
 
     })
 
     it("Update product owner", async () => {
       transaction3 = await product.connect(buyer).getProduct(ID)
-      const pro = { id: transaction3[0], companyName: transaction3[1], price: transaction3[2], currentOwner: transaction3[3], owners: transaction3[4] }
-
+      const pro = { id: transaction3[0], productName: transaction3[1], price: transaction3[2], productImg: transaction3[3], productQr: transaction3[4], currentOwner: transaction3[5], owners: transaction3[6], autheticate: transaction3[7] }
+      const proOnwersArray = [pro.owners[0].userAdd, pro.owners[1].userAdd]
       expect(pro.currentOwner).to.equal(buyer.address)
-      expect(pro.owners).to.have.ordered.members([deployer.address, buyer.address])
+      expect(proOnwersArray).to.have.ordered.members([deployer.address, buyer.address])
     })
 
-    it("Emits sold event", () => {
+    it("Emits product sold event", () => {
       expect(transaction2).to.emit(product, "Sold")
     })
   })
 
-  describe("ViewProducts", () => {
-    let transaction0, transaction1, transaction2, transaction3, transaction;
+  describe("AuthenticateProduct", () => {
+    let transaction, transaction0, transaction1, transaction2, transaction3, transaction4;
 
-    it("Show products", async () => {
-      //view a product
-      transaction0 = await product.connect(deployer).addProduct(COMPANYNAME, PRICE)
-      await transaction0.wait()
-      transaction1 = await product.connect(deployer).addProduct('intel', 100)
-      await transaction1.wait()
-      transaction = await product.connect(deployer).updateProductData(2, buyer.address)
+    beforeEach(async () => {
+      // add users
+      transaction = await product.connect(deployer).addUser(USERNAME, EMAIL, ROLE)
       await transaction.wait()
-      transaction2 = await product.connect(buyer).getProduct(ID)
-      transaction3 = await product.connect(buyer).getProduct(2)
-      const item = { id: transaction3[0], companyName: transaction3[1], price: transaction3[2], currentOwner: transaction3[3], owners: transaction3[4] }
-      const pro = { id: transaction2[0], companyName: transaction2[1], price: transaction2[2], currentOwner: transaction2[3], owners: transaction2[4] }
-      expect(pro.companyName).to.equal(COMPANYNAME)
-      expect(pro.price).to.equal(PRICE)
-      expect(pro.currentOwner).to.equal(deployer.address)
-      expect(pro.owners).to.have.ordered.members([deployer.address])
-      console.log(item);
+      transaction0 = await product.connect(buyer).addUser("buyer", "buyer@gamil.com", "Customer")
+      await transaction0.wait()
+      // add a product
+      transaction1 = await product.connect(deployer).addProduct(PRODUCTNAME, PRICE, ID, PRODUCTIMG, PRODUCTQR)
+      await transaction1.wait()
+      //update a product
+      transaction2 = await product.connect(deployer).updateOwner(ID, buyer.address)
+      await transaction2.wait()
+      //authenticate product
+      transaction3 = await product.connect(buyer).authenticateProduct(ID)
+      await transaction3.wait()
+
     })
+
+    it("Authenticate product", async () => {
+      transaction4 = await product.connect(buyer).getProduct(ID)
+      const pro = { id: transaction4[0], productName: transaction4[1], price: transaction4[2], productImg: transaction4[3], productQr: transaction4[4], currentOwner: transaction4[5], owners: transaction4[6], autheticate: transaction4[7] }
+
+      expect(pro.autheticate).to.equal(true)
+
+    })
+
   })
 
 })
